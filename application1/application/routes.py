1d97d73419d10c9bea9364da0f05bd61444ea0a4
fabiafowler <fabiafowler@gmail.com>
2020-05-05 13:40:09 +0000

    gave second iteration option for username and prize

--- application1/application/routes.py
+++ application1/application/routes.py
@@ -5,15 +5,25 @@ import requests
 
 @app.route('/', methods=['GET'])   
 def home():
+#    three_lower = requests.get('http://application2:5001/generator/three_lower')
+#    get_sixdig = requests.get('http://application3:5002/generator/sixdig')
+#    username = three_lower.text+str(get_sixdig.text)
+#    return render_template('home.html', title='home', username=username)
 
-    three_lower = requests.get('http://application2:5001/generator/three_lower')	 
-    get_sixdig = requests.get('http://application3:5002/generator/sixdig')
-    username = three_lower.text+str(get_sixdig.text)
-    return render_template('home.html', title='home', username=username) 
+# second iteration
+    two_upper = requests.get('http://application2:5001/generator/two_upper')
+    get_eightdig = requests.get('http://application3:5002/generator/eightdig')
+    username = two_upper.text+str(get_eightdig.text)
+    return render_template('home.html', title='home', username=username)
 
-
+# first iteration
 @app.route('/prize/<prizeusername>', methods=['GET', 'POST'])
-def prize(prizeusername):
-    prize = requests.post('http://application4:5003/prizeshort', data=prizeusername)
-    return render_template('prize.html', title='prize', prizeusername=prizeusername, prize=prize.text)
+#def prize(prizeusername):
+#    prize = requests.post('http://application4:5003/prizeshort', data=prizeusername)
+#    return render_template('prize.html', title='prize', prizeusername=prizeusername, prize=prize.text)
 
+
+### second iteration
+def prize(prizeusername):
+    prize = requests.post('http://application4:5003/prizelong', data=prizeusername)
+    return render_template('prize.html', title='prize', prizeusername=prizeusername, prize=prize.text)
