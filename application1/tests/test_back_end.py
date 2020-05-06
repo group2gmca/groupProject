a352ea7fc049f84a7ffb866d35e88aaac1f096d0
fabiafowler <fabiafowler@gmail.com>
2020-05-06 15:15:21 +0000

    starting testing, will move to new branch to collect

--- application1/tests/test_back_end.py
+++ application1/tests/test_back_end.py
@@ -1,9 +1,48 @@
 import unittest
 
 from flask import abort, url_for
-from flask_testing import TestCase, TestBase
+from flask_testing import TestCase
 
-from application import app
+from application import app, db
+from application.models import Prize
+class TestBase(TestCase):
+
+    def create_app(self):
+
+        # pass in configurations for test database
+        config_name = 'testdb'
+        app.config.update(
+                SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@localhost:3306/testdb'        )
+
+        return app
+
+    def setUp(self):
+        """
+        Will be called before every test
+        """
+        # ensure there is no data in the test database when the test starts
+        db.session.commit()
+        db.drop_all()
+        db.create_all()
+
+        # create test admin user
+        test1 = Prize(id=1, username="abc12345", prize="£10 gift card")
+
+        # create test non-admin user
+        test2 = Prize(id=2, username="123abcde", prize="£30 gift card")
+        # save users to database
+        db.session.add(test1)
+        db.session.add(test2)
+        db.session.commit()
+
+    def tearDown(self):
+        """
+        Will be called after every test
+        """
+
+        db.session.remove()
+        db.drop_all()
+
 
 class TestViews(TestBase):
 
@@ -11,5 +50,6 @@ class TestViews(TestBase):
         """
         Test that homepage is accessible without login
         """
-        response = self.client.get(url_for('home'))
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
